bb6a7018d8f1a8d44df15997fa3566295d0cf43f
guilherme C Santos <guicarsiqsantos@gmail.com>
2019-11-26 23:08:21 -0300

    alteraçao

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const app = express();
-
 require('dotenv').config();
+
+const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
